066e1ce809a69132e8c5e6522b2b07e95f746e7e
haigang-shushu <haigangshushu@gmail.com>
2022-08-28 10:29:57 +0800

    py 100题 提交第一题 更新另一个算法

--- 01. 组成多少个互不相同而且无重复的三位数.py
+++ 01. 组成多少个互不相同而且无重复的三位数.py
@@ -1,12 +1,15 @@
 # -*- coding:UTF-8 -*-
 # 有四个数字: 1, 2, 3, 4, 能组成互不相同而且无重复数字的三位数？各是多少？
 # 在百位, 十位, 个位的数字都是 1, 2, 3, 4。 组成所有的排列后再去掉不满足的条件的排列
+
+# 解法1
 for i in range(1, 5):
     for j in range(1, 5):
         for k in range(1, 5):
             if(i != k) and (i != j) and (j != k):
                 print(i, j, k)
 
+# 解法2
 for a in [1, 2, 3, 4]:
     for b in [1, 2, 3, 4]:
         for c in [1, 2, 3, 4]:
