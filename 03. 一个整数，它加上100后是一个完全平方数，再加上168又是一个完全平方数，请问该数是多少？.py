d0bcd6fff52b82677952caa1242deabeb0be4c17
haigang-shushu <haigangshushu@gmail.com>
2022-08-30 22:53:31 +0800

    py 100题 提交第三题

--- 03. 一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？.py
+++ 03. 一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？.py
@@ -13,7 +13,7 @@ for i in range(-168, 169):
     for j in range(-168, 169):
         m = (j - i) / 2
         n = (i + j) / 2
-        if i * j == 168 and (n -m) * (n + m) == 168:
+        if i * j == 168 and (n - m) * (n + m) == 168:
             # 整数
             if (m ** 2 - 100) % 1 == 0:
                 x.append(m ** 2 - 100)
